a7026896cd29f531f95254334dfef732253f405d
Gabriel Kuroda <gabrielkuroda20@gmail.com>
2022-02-16 20:17:05 -0300

    add print board function

--- dama.py
+++ dama.py
@@ -1,3 +1,5 @@
+board = [['  ' for i in range(8)] for i in range(8)]
+
 def get_player_option():
     while True:
         player_option = input("Digite 1 para jogar contra a máquina e 2 para jogar contra outro jogador: ")
@@ -5,14 +7,26 @@ def get_player_option():
         if player_option not in ["1", "2"]:
             print("Opção invalida!")
         else:
+            printBoard()
             break
         
     return player_option
 
+def printBoard():
+    print("------------------")
+    print("  0  1  2  3  4  5  6  7")
+    for idxLine,line in enumerate(board):
+        for idxColumn,column in enumerate(line):
+            if idxColumn is 0:
+                print(str(idxLine)+"|"+column, end = "|")
+            elif idxColumn is 7:
+                print(column+"|")
+            else:
+                print(column, end = "|")
+    print("------------------")
 
 def main():
     player_otion = get_player_option()
     print(player_otion)
-
 
 main()
